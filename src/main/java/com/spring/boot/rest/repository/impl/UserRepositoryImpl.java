04055e9e00a6bb7153c3567b4ae677bc426e7081
Vitor Bastos <vitorbastosbn@gmail.com>
2018-02-22 21:15:00 -0300

    -- Atualização da injeção de repository

--- src/main/java/com/spring/boot/rest/repository/impl/UserRepositoryImpl.java
+++ src/main/java/com/spring/boot/rest/repository/impl/UserRepositoryImpl.java
@@ -2,9 +2,12 @@ package com.spring.boot.rest.repository.impl;
 
 import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
 import com.spring.boot.rest.entity.User;
 import com.spring.boot.rest.repository.UserRepositoryCustom;
 
+@Repository("ur")
 public class UserRepositoryImpl implements UserRepositoryCustom {
 
 	@Override
